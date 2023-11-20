ec90486af797ffdaa81bf3c317d0593436c446bc
YoussefGhollamallah <youssef.gh@live.fr>
2023-11-20 11:43:35 +0100

    update job 9

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -9,3 +9,9 @@ produit = {
 
 affichage_produit = f"les information du produit en vente sont : nom = {produit['nom']}, sont prix est de {produit['prix']} euros/unitaire et la quantiter du stock est de {produit['quantité en stock']}"
 print(affichage_produit)
+
+user_acheteur = int(input(f"Combien de {produit['nom']} voulez vous acheter ? "))
+
+quantiter_acheter = int(user_acheteur)
+
+print(f" vous acheter {quantiter_acheter} {produit['nom']} du coup il reste {int(produit['quantité en stock']) - quantiter_acheter} {produit['nom']} en stock")
