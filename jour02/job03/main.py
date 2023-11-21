176fb070dcd652bdce77660217f101c8629362e3
YoussefGhollamallah <youssef.gh@live.fr>
2023-11-21 15:47:56 +0100

    update

--- jour02/job03/main.py
+++ jour02/job03/main.py
@@ -10,3 +10,12 @@ numbers_list.remove(37)
 numbers_list.remove(88)
 
 print(numbers_list)
+
+j = 0
+
+while j <= 100: 
+    if j == 26 or j == 37 or j == 88:
+        j += 1
+        continue
+    print(j)
+    j += 1
