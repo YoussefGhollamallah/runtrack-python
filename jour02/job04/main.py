def93468351bd2a8d2f800994b9b3b91e965470b
YoussefGhollamallah <youssef.gh@live.fr>
2023-11-21 13:55:50 +0100

    job 4 terminer

---
+++ jour02/job04/main.py
@@ -0,0 +1,9 @@
+nombre = int(input("Veuillez entrez un nombre : "))
+
+
+for i in range(1,10):
+    if nombre != 0:
+        print(f" - {i} x {nombre} = {i * nombre}")
+    else : 
+        nombre = int(input("Veuillez entrez un nombre : ")) 
+        
