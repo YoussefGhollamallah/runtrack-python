a474366dfaf01f17e0339714b46e17379c8f5a84
YoussefGhollamallah <youssef.gh@live.fr>
2023-11-21 13:58:08 +0100

    job 6 terminer

---
+++ jour02/job06/main.py
@@ -0,0 +1,9 @@
+# calcul les nombre premier
+
+for n in range(0,1000 + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
